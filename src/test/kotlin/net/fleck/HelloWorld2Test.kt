e8fda1bd248bd8146f0d01cb8320b1178f472264
a1flecke <aaron.fleckenstein@daveramsey.com>
2020-03-20 10:49:42 -0500

    Actually get tests to fail

--- src/test/kotlin/net/fleck/HelloWorld2Test.kt
+++ src/test/kotlin/net/fleck/HelloWorld2Test.kt
@@ -2,8 +2,9 @@ package net.fleck
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.lang.RuntimeException
 
-class HelloWorldTest{
+class HelloWorld2Test{
     @Test
     fun helloWorld() {
         assertThat(HelloWorld().hello("name")).isEqualTo("")
