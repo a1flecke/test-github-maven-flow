dca66eae4a07babd51fca88b0769be9afd0864f2
a1flecke <aaron.fleckenstein@daveramsey.com>
2020-03-20 11:12:07 -0500

    Add useless if else

--- src/main/kotlin/net/fleck/HelloWorld.kt
+++ src/main/kotlin/net/fleck/HelloWorld.kt
@@ -3,4 +3,8 @@ package net.fleck
 class HelloWorld {
     fun hello(name:String) = "Hello $name"
     fun anotherFunction(name: String) = "This is a good change: $name"
+    fun uselessIfElse() :String = if(true)
+            "It was true"
+    else
+            "False"
 }
